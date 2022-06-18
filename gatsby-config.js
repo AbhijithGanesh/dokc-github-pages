0575f8adbcf713b49388dad958aff8c579041638
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-06-18 13:37:32 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    title: `community-pages`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-mdx", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
