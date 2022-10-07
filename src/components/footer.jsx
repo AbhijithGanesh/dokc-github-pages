6a45920705d242b617e797e52a0ea435fc6fc5b7
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-10-07 21:33:16 +0530

    Open Source commit
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,11 +3,11 @@ import * as React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className="my-10  text-gray-400 font-semibold">
+      <footer className="mt-10  text-gray-400 font-semibold">
         <section className="grid lg:grid-cols-10 sm:grid-cols-1 md:grid-cols-1 gap-2">
           <section className="flex flex-auto bg-white justify-between lg:col-start-3 col-span-6 h-[0.2px] w-auto lg:text-lg md:px-4 sm: text-sm">
-            <p>Copyrights reserved © {new Date().getFullYear()}</p>
-            <p> Data on Kuberenetes Community </p>
+            <section>Copyrights reserved © {new Date().getFullYear()}</section>
+            <section> Data on Kuberenetes Community </section>
           </section>
         </section>
       </footer>
--- src/pages/guide-to-open-source.jsx
+++ src/pages/guide-to-open-source.jsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-import image from "../../images/dokc-logo-white.svg";
+import { RiOpenSourceFill } from "react-icons/ri";
+import image from "./../images/dokc-logo-white.svg";
 
-let LandingPage = () => {
+let Guide = () => {
   return (
     <>
       <section className="pt-8 grid lg:grid-cols-10 sm:grid-cols-1 gap-6">
@@ -15,7 +16,7 @@ let LandingPage = () => {
           </section>
         </section>
         <section className="sm:col-start-1 lg:text-right lg:col-start-5 lg:col-span-4">
-          <section className="px-3 text-white lg:text-4xl md:text-2xl sm:text-lg font-bold capitalize hover:text-white">
+          <section className="px-3 text-white lg:text-4xl md:text-2xl sm:text-lg font-semibold capitalize hover:text-white">
             Welcome to the Data on Kubernetes Community!
             <section className="md:h-[0.5px] lg:h-[2px] w-auto bg-white  my-2" />
             <section className="text-white font-light normal-case lg:text-2xl md:text-xl sm:text-md hover:text-white">
@@ -26,8 +27,18 @@ let LandingPage = () => {
           </section>
         </section>
       </section>
+      <section className="pt-8 grid lg:grid-cols-12">
+        <section className="col-start-3 col-span-8">
+          <section className="text-white font-bold text-4xl items-center">
+            What is Open Source ?
+          </section>
+          <section className="pt-4 text-white font-regular text-xl">
+            #How to get started? #What does it mean ? #Why should you ?
+          </section>
+        </section>
+      </section>
     </>
   );
 };
 
-export default LandingPage;
+export default Guide;
